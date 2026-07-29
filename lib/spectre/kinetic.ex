fe9bd37aac1cf49ff691ac1a0e1a583855c4a207
Yuriy Zhar <darkyfelis@gmail.com>
2026-07-29 03:20:19 +0200

    feat(kinetic): adopt run-native stack contracts

--- lib/spectre/kinetic.ex
+++ lib/spectre/kinetic.ex
@@ -35,9 +35,9 @@ defmodule Spectre.Kinetic do
 
   use Spectre.Stack.Installable,
     id: :kinetic,
-    version: "0.1.2",
+    version: "0.1.3",
     contract: 1,
-    spectre: "~> 0.1.2",
+    spectre: "~> 0.1.3",
     provides: [{:service, :kinetic}],
     agent_extensions: [Spectre.Kinetic.Extension],
     dsl: __MODULE__,
